23b5d845f1ada83d1dc90917c23b6a9af1381a03
mmhan <mmhan2u@gmail.com>
2015-07-23 00:09:56 +0630

    Completed to the point of showing popular movies

--- app/src/main/java/net/mmhan/popularmovies/MainActivity.java
+++ app/src/main/java/net/mmhan/popularmovies/MainActivity.java
@@ -2,13 +2,28 @@ package net.mmhan.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import net.mmhan.popularmovies.model.MovieService;
 import net.mmhan.popularmovies.model.MoviesResult;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -18,10 +33,26 @@ public class MainActivity extends AppCompatActivity {
 
     public final String LOG_TAG = this.getClass().getName();
 
+    @Bind(R.id.rview_grid)
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+
+    ArrayList<MoviesResult.Movie> mMovies;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mMovies = new ArrayList<>();
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MovieThumbnailAdapter(mMovies);
+        mRecyclerView.setAdapter(mAdapter);
+
 
         MovieService.Implementation
                 .get(getString(R.string.api_key))
@@ -29,12 +60,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void success(MoviesResult moviesResult, Response response) {
                         for(MoviesResult.Movie m : moviesResult.getMovies()){
+                            mMovies.add(m);
+                            mAdapter.notifyDataSetChanged();
                             Log.e(LOG_TAG, "Movie: " + m.title);
                         }
                     }
                     @Override
                     public void failure(RetrofitError error) {
-
+                        Log.e(LOG_TAG, error.toString());
                     }
                 });
     }
@@ -59,5 +92,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class MovieThumbnailAdapter extends RecyclerView.Adapter<MainActivity.MovieThumbnailAdapter.MovieThumbnailViewHolder>{
+
+        List<MoviesResult.Movie> mData;
+
+        class MovieThumbnailViewHolder extends RecyclerView.ViewHolder{
+
+            @Bind(R.id.tv_title)
+            public TextView mTextView;
+            @Bind(R.id.iv_thumbnail)
+            public ImageView mImageView;
+
+            public MovieThumbnailViewHolder(View v) {
+                super(v);
+                ButterKnife.bind(this, v);
+                mTextView.getText();
+            }
+        }
+
+
+        public MovieThumbnailAdapter(List<MoviesResult.Movie> movies){
+            mData = movies;
+        }
+
+        @Override
+        public MovieThumbnailViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // inflate a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.thumbnail_view, parent, false);
+            MovieThumbnailViewHolder vh = new MovieThumbnailViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(MovieThumbnailViewHolder holder, int position) {
+            holder.mTextView.setText(mData.get(position).title);
+            //IMAGE
+            Glide.with(holder.mImageView.getContext())
+                    .load(mData.get(position).getPosterUrl())
+//                    .error(R.drawable.placeholder) TODO
+//                    .placeholder(R.drawable.placeholder) TODO
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(holder.mImageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
     }
 }
--- app/src/main/java/net/mmhan/popularmovies/model/MoviesResult.java
+++ app/src/main/java/net/mmhan/popularmovies/model/MoviesResult.java
@@ -17,10 +17,18 @@ public class MoviesResult {
     }
 
     public class Movie {
-        public String poster_path;
+
+        String poster_path;
         public String title;
         public String overview;
         public Float vote_average;
         public String release_date;
+
+        final String POSTER_SIZE = "w154";
+        final String BASE_URL = "http://image.tmdb.org/t/p/";
+
+        public String getPosterUrl(){
+            return String.format("%s%s/%s", BASE_URL, POSTER_SIZE, poster_path);
+        }
     }
 }
