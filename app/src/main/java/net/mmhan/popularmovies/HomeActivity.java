c2da5d8925dd7d96756f2d8e1961776a973109f0
mmhan <mmhan2u@gmail.com>
2015-09-30 20:48:41 +0630

    Added fragment holder activity

---
+++ app/src/main/java/net/mmhan/popularmovies/HomeActivity.java
@@ -0,0 +1,38 @@
+package net.mmhan.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by mmhan on 30/9/15.
+ */
+public class HomeActivity extends AppCompatActivity{
+
+
+    @Bind(R.id.phone_container)
+    FrameLayout frameLayout;
+
+    boolean isPhone = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ButterKnife.bind(this);
+
+        if(frameLayout == null){
+            //this is tablet
+            isPhone = false;
+        }else{
+            if(savedInstanceState == null){
+
+            }
+        }
+    }
+}
